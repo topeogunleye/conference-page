c158d348b72f765ef0cf1fd6e5116f310e6efe48
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-11-16 09:43:48 +0100

    Add Home page markup and style

---
+++ script.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', () => {
+// https://codepen.io/ljc-dev/embed/GRoLWxj?height=600&default-tab=js,result&embed-version=2
+// https://dev.to/ljcdev/easy-hamburger-menu-with-js-2do0
+const navbar = document.querySelector('.navbar'); // list
+const ham = document.querySelector('.ham'); // button
+const menuLinks = document.querySelectorAll('.menu-link');
+
+function toggleHamburger() {
+  navbar.classList.toggle('showNav');
+  ham.classList.toggle('showClose');
+}
+
+// toggles hamburger menu in and out when clicking on the hamburger
+ham.addEventListener('click', toggleHamburger);
+
+// toggle when clicking on links
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener('click', toggleHamburger);
+});
+
+})
